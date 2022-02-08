5b61064a22da7c23faa877d69897c78d772478b9
jorlane.melo@gmail.com <jorlane.melo@gmail.com>
2022-02-08 11:56:08 -0300

    teste .env

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const express = require('express')
 const app = express()
 
-const db = require('./db')
+//const db = require('./db')
 
 const port = process.env.PORT || 3000
 
+const DATABASE    = process.env.DATABASE
+
 app.get('/', (req, res) => {
-    res.send('Oi Jorlane')
+    res.send('Oi Jorlane. Database ==> ' + DATABASE)
 })
 
 app.listen(port, async() => {
     console.log('Iniciando o Banco de Dados. Aguarde....')
-    try {
-        await db.sync({force: false})
-        await start.initDataBase()
+    console.log(`O servidor está executando na porta ${port}...          `)
 
-        console.log(`O servidor está executando na porta ${port}...          `)
-    } catch (e) {
-        console.log('Erro ao tentar conectar com o banco de dados: ')
-    }
+    // try {
+    //     await db.sync({force: false})
+    //     await start.initDataBase()
+
+    //     console.log(`O servidor está executando na porta ${port}...          `)
+    // } catch (e) {
+    //     console.log('Erro ao tentar conectar com o banco de dados: ')
+    // }
 })
