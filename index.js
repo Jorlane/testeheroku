bb7cc80655c32553cac4e45cf80826798339fb0e
jorlane.melo@gmail.com <jorlane.melo@gmail.com>
2022-02-08 10:42:37 -0300

    altera index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 
+const port = process.env.PORT || 3000
+
 app.get('/', (req, res) => {
     res.send('Oi Jorlane')
 })
 
-app.listen(3000, () => {
-    console.log('Server listening at port 3000...')
+app.listen(port, () => {
+    console.log(`Server listening at port 3000...${port}`)
 })
