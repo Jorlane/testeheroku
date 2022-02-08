8d3e2d61010281bcf3f65de49226aed7d1539d96
jorlane.melo@gmail.com <jorlane.melo@gmail.com>
2022-02-08 17:13:50 -0300

    testa db

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-//const db = require('./db')
+const db = require('./db')
 
 const port = process.env.PORT || 3000
 
@@ -15,12 +15,11 @@ app.listen(port, async() => {
     console.log('Iniciando o Banco de Dados. Aguarde....')
     console.log(`O servidor está executando na porta ${port}...          `)
 
-    // try {
-    //     await db.sync({force: false})
-    //     await start.initDataBase()
+    try {
+        await db.sync({force: false})
 
-    //     console.log(`O servidor está executando na porta ${port}...          `)
-    // } catch (e) {
-    //     console.log('Erro ao tentar conectar com o banco de dados: ')
-    // }
+        console.log(`O servidor está executando na porta ${port}...          `)
+    } catch (e) {
+        console.log('Erro ao tentar conectar com o banco de dados: ', e)
+    }
 })
