665210aa32e04978e404e7dca8ec44a8f06de4cc
jorlane.melo@gmail.com <jorlane.melo@gmail.com>
2022-02-09 11:26:46 -0300

    teste com porta

--- db.js
+++ db.js
@@ -5,12 +5,15 @@ const DATABASE    = process.env.DATABASE
 const DB_USER     = process.env.DB_USER
 const DB_PASSWORD = process.env.DB_PASSWORD
 const DB_SERVER   = process.env.DB_SERVER
+const DB_PORT     = process.env.DB_PORT
 const DB_HOST     = process.env.DB_HOST
 
-const sequelize   = new Sequelize(DATABASE, DB_USER, DB_PASSWORD, {
-    dialect: DB_SERVER, 
-    host: DB_HOST
-})
+const sequelize = new Sequelize(`${DB_SERVER}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DATABASE}`) 
+
+// const sequelize   = new Sequelize(DATABASE, DB_USER, DB_PASSWORD, {
+//     dialect: DB_SERVER, 
+//     host: DB_HOST
+// })
 
 //testando a conexão
 async function testConnection() {
